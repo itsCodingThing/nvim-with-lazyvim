8a4a7994cd087771e7bbd26b697b9f0a2853531e
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2024-09-04 15:57:47 +0530

    neovim with lazyvim distro

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,22 @@
+return {
+  -- -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  --
+  -- -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
